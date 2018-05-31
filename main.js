f1c239637443b25713c8935e181cfa4fd3d8e83b
Jerome Chavez <jayrome12@yahoo.com>
2018-05-31 14:50:36 -0700

    Defined function to render all characters in the phrase

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 var phraseString = 'grumpy wizards make toxic brew for the evil queen and jack'
 
-var characterStrings = phraseString.split('').map((char) => {
-  return {char: char}
-})
+function createPhrase(string) {
+  phrase = []
+  for (var i = 0; i < string.length; i++) {
+    phrase.push({char: string.charAt(i)})
+  }
+  return phrase
+}
+
+var phraseArray = createPhrase(phraseString)
 
 function renderCharacter (phrase) {
-  $phraseElement = document.createElement('p')
-  $phraseElement.textContent = phrase[0].char
+  $phraseElement = document.createElement('span')
+  $phraseElement.textContent = phrase.char
   return $phraseElement
 }
+
+function renderAll(phrase) {
+  phrase.forEach(function (object) {
+    document.body.appendChild(renderCharacter(object))
+  })
+}
+renderAll(phraseArray)
