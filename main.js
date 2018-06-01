7cfca4a1255db9698f571f15d2f0b497c75ab24c
Jerome Chavez <jayrome12@yahoo.com>
2018-06-01 13:00:46 -0700

    Added conditionals to compared keydown value to currentcharacter value and to record failures if not matched

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var phraseString = 'grumpy wizards make toxic brew for the evil queen and jack'
 function createPhrase(string) {
   phrase = []
   for (var i = 0; i < string.length; i++) {
-    phrase.push({char: string.charAt(i), index: i})
+    phrase.push({char: string.charAt(i), index: i, failures: 0})
   }
   return phrase
 }
@@ -12,23 +12,41 @@ var phraseArray = createPhrase(phraseString)
 
 var appState = {
   char: phraseArray,
-  currentCharacter: 0,
-  failures: 0
+  currentCharacter: phraseArray[0].char,
+  currentIndex: 0
 }
 
 function renderCharacter (phrase) {
   $phraseElement = document.createElement('span')
   $phraseElement.textContent = phrase.char
-  if (phrase.index === appState.currentCharacter) {
+  if (phrase.index === appState.currentIndex) {
     $phraseElement.classList.add('current-character')
   }
   return $phraseElement
 }
 
 function renderAll(phrase) {
-  phrase.forEach(function (object) {
-    document.body.appendChild(renderCharacter(object))
+  $phraseAll = document.createElement('div')
+  $phraseAll.classList.add('phrase')
+
+  appState.char.forEach(function (object) {
+    $phraseAll.appendChild(renderCharacter(object))
   })
+  document.body.appendChild($phraseAll)
 }
 
-window.addEventListener('keydown', renderAll(phraseArray))
+renderAll(phraseArray)
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === appState.currentCharacter) {
+    appState.currentIndex++
+    appState.currentCharacter = phraseArray[appState.currentIndex].char
+  }
+  else {
+    phraseArray[appState.currentIndex].failures++
+  }
+
+  var $update = document.querySelector('.phrase')
+  $update.remove()
+  renderAll(phraseArray)
+})
